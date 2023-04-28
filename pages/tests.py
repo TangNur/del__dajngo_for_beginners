087c1a624a40cb64a16de755118992df4da77ad8
TangNur <wolfamadeus@gmail.com>
2023-04-28 12:29:36 +0600

    intital

---
+++ pages/tests.py
@@ -0,0 +1,11 @@
+from django.test import SimpleTestCase
+
+
+class SimpleTestCases(SimpleTestCase):
+    def test_home_page_response_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_response_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
