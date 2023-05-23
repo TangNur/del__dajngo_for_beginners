da0368c0e009756d4ee0333ac083ac196b00e776
TangNur <wolfamadeus@gmail.com>
2023-05-23 11:30:06 +0600

    ffwaESf4

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from blog.models import PostTab
 
@@ -11,3 +13,21 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = PostTab
     template_name = 'blog/post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = PostTab
+    template_name = 'blog/post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = PostTab
+    template_name = 'blog/post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = PostTab
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('home')
